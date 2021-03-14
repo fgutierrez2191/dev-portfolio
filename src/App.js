38dad242a613e325b6a9d718fe0466bd94f20f48
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-03-13 21:48:59 -0500

    app renders

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
+
