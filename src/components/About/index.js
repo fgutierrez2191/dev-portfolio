e590ca51bf588eb33b4cd3a101a5cd1f717928a1
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-04-02 17:11:52 -0400

    added personal brand statement

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,7 @@ function About() {
     return (
       <section>
         <h1 id="about">About Me</h1>
+        <p>Productive and results driven Web Developer with a passion for understanding systems. I leverage my experience in two different career fields to dive deep into new technologies and learn them thoroughly.I recently recieved a certificate in Full-Stack Web Development from The University of Texas at Austin. Here I learned the Mern Stack and got comfortable with Javascript, Node.js, Express, Handlebars.js, Sql and MySql databases, MongoDb, Css and Html. I have a passion for optimizing systems and improvement through innovation and I am looking to bring my technical skills to a company that will use them. </p>
       </section>
     );
   }
